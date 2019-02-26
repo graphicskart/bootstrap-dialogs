0ef145914d4a83ec134898dafe283a2910698894
graphicskart <graphicskart2018@gmail.com>
2019-02-26 20:54:19 +0530

    Updates

---
+++ precache-manifest.09971580b35b2b73f7fe2bf4327f22c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c347949c7cc1c22fba84",
+    "url": "/bootstrap-dialogs/static/js/runtime~main.c347949c.js"
+  },
+  {
+    "revision": "e37c669109671d5b7215",
+    "url": "/bootstrap-dialogs/static/js/main.e37c6691.chunk.js"
+  },
+  {
+    "revision": "eeb3cea3de826c9dab82",
+    "url": "/bootstrap-dialogs/static/js/2.eeb3cea3.chunk.js"
+  },
+  {
+    "revision": "e37c669109671d5b7215",
+    "url": "/bootstrap-dialogs/static/css/main.dbbefb49.chunk.css"
+  },
+  {
+    "revision": "f88c5e0435a0ab63e7761ecb4ca1073f",
+    "url": "/bootstrap-dialogs/index.html"
+  }
+];
